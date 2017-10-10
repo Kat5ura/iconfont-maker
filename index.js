d991ae67d22c276da9073c91061f2870b0cee03c
Kat5ura <971969499@qq.com>
2017-10-10 16:58:51 +0800

    1. add editor config file 2. modify done callback function 3. Edit README.md

--- index.js
+++ index.js
@@ -2,43 +2,38 @@ const glob = require('glob');
 const generator = require('webfonts-generator');
 
 let iconfont = function (options, done) {
-    let files = options.files;
+	let files = options.files;
 
-    if (!files || !files.length) {
-        throw (new Error('You need to specify icon files'));
-    }
+	if (!files || !files.length) {
+		throw (new Error('You need to specify icon files'));
+	}
 
-    if (Object.prototype.toString.apply(files) === '[object String]') {
-        files = [files];
-    }
+	if (Object.prototype.toString.apply(files) === '[object String]') {
+		files = [files];
+	}
 
-    if (!Array.isArray(files)) {
-        throw (new Error('Files options only accept String or Array'));
-    }
+	if (!Array.isArray(files)) {
+		throw (new Error('Files options only accept String or Array'));
+	}
 
-    let allFiles = [];
-    files.forEach((item) => {
-        try {
-            let res = glob.sync(item);
-            allFiles = [...allFiles, ...res];
-        } catch (error) {
-            throw (error)
-        }
-    });
+	let allFiles = [];
+	files.forEach((item) => {
+		try {
+			let res = glob.sync(item);
+			allFiles = [...allFiles, ...res];
+		} catch (error) {
+			throw (error)
+		}
+	});
 
-    options.files = allFiles.filter((file) => {
-        let reg = /\.svg$/ig;
-        return reg.test(file);
-    });
+	options.files = allFiles.filter((file) => {
+		let reg = /\.svg$/ig;
+		return reg.test(file);
+	});
 
-    generator(options, function (err, res) {
-        if (err) {
-            console.log('Fail!', err);
-        } else {
-            console.log('Done!');
-            Object.prototype.toString.apply(done) === '[object Function]' && done(res);
-        }
-    });
+	generator(options, function (err, res) {
+		Object.prototype.toString.apply(done) === '[object Function]' && done(err, res);
+	});
 };
 
 module.exports = iconfont;
